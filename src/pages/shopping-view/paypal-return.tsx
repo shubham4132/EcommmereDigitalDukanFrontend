6e8a90398ff522ff5919eeae97d9ee7b8022ec9d
shubham4132 <shubhamg3122134@gmail.com>
2026-03-25 16:50:09 +0530

    change added

--- src/pages/shopping-view/paypal-return.tsx
+++ src/pages/shopping-view/paypal-return.tsx
@@ -1,12 +1,12 @@
 import { capturePayment } from "@/store/shop/order-slice";
-import type { AppDispatch } from "@/store/store";
+
 import { Card, CardHeader, CardTitle } from "components/ui/card";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useLocation } from "react-router";
 
 function PaypalReturnPage() {
-  const dispatch = useDispatch<AppDispatch>();
+  const dispatch = useDispatch();
   const location = useLocation();
   const params = new URLSearchParams(location.search);
   const paymentId = params.get("paymentId");
@@ -22,7 +22,7 @@ function PaypalReturnPage() {
       }
       const orderId: string = JSON.parse(storedOrderId);
       dispatch(capturePayment({ paymentId, payerId, orderId })).then((data) => {
-        if ((data?.payload as any)?.success) {
+        if (data?.payload?.success) {
           sessionStorage.removeItem("currentOrderId");
           window.location.href = "/shop/payment-success";
         }
